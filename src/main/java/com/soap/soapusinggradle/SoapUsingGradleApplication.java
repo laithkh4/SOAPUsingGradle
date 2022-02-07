928651bd44a9b1d742d23d64f1fc49ba0df7bcd0
Laith Abu Khaizaran <LaithAK@exalt.ps>
2022-02-07 15:55:38 +0200

    Finished Example of SOAP Endpoints !

---
+++ src/main/java/com/soap/soapusinggradle/SoapUsingGradleApplication.java
@@ -0,0 +1,13 @@
+package com.soap.soapusinggradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapUsingGradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoapUsingGradleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/soap/soapusinggradle/SoapUsingGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soap.soapusinggradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapUsingGradleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
